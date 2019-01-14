6c96ec8386d38808a9e1265d2f4dbfa51a2f49ce
GuoRui <guorui@huami.com>
2019-01-14 19:10:39 +0800

    fix |> 修改GZTableViewCell的item属性修饰符为strong.因为item上增加了当前cell作为监听器，用weak的话会导致，item先被释放，但是item上的监听器(当前cell并没有被释放)，会导致崩溃。所以，必须用strong，当cell被释放时，item才会被释放。这样就合理了。这个问题好像会在iOS10系统上表现出来。iOS12没有此问题。

--- GZTableViewManager/Classes/GZTableViewCell.h
+++ GZTableViewManager/Classes/GZTableViewCell.h
@@ -25,7 +25,15 @@ typedef NS_ENUM(NSUInteger, GZTableViewCellType) {
 
 @property (weak, nonatomic) GZTableViewManager  *tableViewManager;
 @property (weak, nonatomic) GZTableViewSection  *section;
-@property (weak, nonatomic) GZTableViewItem     *item;
+
+/**
+ 修饰item, 必须用strong，不能用weak。
+ 
+ 因为item上增加了当前cell作为监听器，用weak的话会导致，item先被释放，但是item上的监听器(当前cell并没有被释放)，会导致崩溃。所以，必须用strong，当cell被释放时，item才会被释放。这样就合理了。
+ 
+ 这个问题好像会在iOS10系统上表现出来。iOS12没有此问题。
+ */
+@property (strong, nonatomic) GZTableViewItem     *item;
 @property (readonly) GZTableViewCellType        cellType;
 
 @property (strong, nonatomic) NSIndexPath *indexPath;
