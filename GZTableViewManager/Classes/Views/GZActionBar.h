0cc42359452a166d0ea2c2cbb271435d3ce6af0a
Guorui <xyt_gr@126.com>
2020-01-26 22:52:55 +0800

    feat |> 增加actionbar

---
+++ GZTableViewManager/Classes/Views/GZActionBar.h
@@ -0,0 +1,16 @@
+//
+//  GZActionBar.h
+//  GZTableViewManager
+//
+//  Created by GR Harry on 2020/1/26.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GZActionBar : UIToolbar
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GZTableViewManager/Classes/Views/GZActionBar.m
@@ -0,0 +1,20 @@
+//
+//  GZActionBar.m
+//  GZTableViewManager
+//
+//  Created by GR Harry on 2020/1/26.
+//
+
+#import "GZActionBar.h"
+
+@implementation GZActionBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
